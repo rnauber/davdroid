3f70bb2117150560b2d393eefeed1dde736ec4e2
Your Name <you@example.com>
2014-12-21 12:40:55 +0100

    x

--- src/at/bitfire/davdroid/webdav/DavHttpClient.java
+++ src/at/bitfire/davdroid/webdav/DavHttpClient.java
@@ -56,6 +56,10 @@ public class DavHttpClient {
 				.setUserAgent("DAVdroid/" + Constants.APP_VERSION)
 				.disableCookieManagement();
 		
+		HttpHost proxy = new HttpHost("127.0.0.1", 8080);		
+		builder=builder.setProxy(proxy);
+		
+		
 		if (disableCompression) {
 			Log.d(TAG, "Disabling compression for debugging purposes");
 			builder = builder.disableContentCompression();
